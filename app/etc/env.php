f085bfb8a7f3534f1fbc7267994e371ee0fb2e83
HUYNH NHU Y <156324430+hnyii@users.noreply.github.com>
2024-01-12 18:52:49 +0700

    first commit

---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'c5EcczMh9DfCMabo6AMhdWsmKwN6FUqZ'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'ca3_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'ca3_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'ab6bc11e26e9d35d8e06ccb7d148a36c'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => '123456',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'huynhnhuy.magento.com'
+    ],
+    'install' => [
+        'date' => 'Thu, 30 Nov 2023 12:51:20 +0000'
+    ]
+];
---
+++ generated/code/Magento/Paypal/Controller/Express/Start/Interceptor.php
@@ -0,0 +1,106 @@
+<?php
+namespace Magento\Paypal\Controller\Express\Start;
+
+/**
+ * Interceptor class for @see \Magento\Paypal\Controller\Express\Start
+ */
+class Interceptor extends \Magento\Paypal\Controller\Express\Start implements \Magento\Framework\Interception\InterceptorInterface
+{
+    use \Magento\Framework\Interception\Interceptor;
+
+    public function __construct(\Magento\Framework\App\Action\Context $context, \Magento\Customer\Model\Session $customerSession, \Magento\Checkout\Model\Session $checkoutSession, \Magento\Sales\Model\OrderFactory $orderFactory, \Magento\Paypal\Model\Express\Checkout\Factory $checkoutFactory, \Magento\Framework\Session\Generic $paypalSession, \Magento\Framework\Url\Helper\Data $urlHelper, \Magento\Customer\Model\Url $customerUrl, ?\Psr\Log\LoggerInterface $logger = null)
+    {
+        $this->___init();
+        parent::__construct($context, $customerSession, $checkoutSession, $orderFactory, $checkoutFactory, $paypalSession, $urlHelper, $customerUrl, $logger);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'execute');
+        return $pluginInfo ? $this->___callPlugins('execute', func_get_args(), $pluginInfo) : parent::execute();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomerBeforeAuthUrl()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getCustomerBeforeAuthUrl');
+        return $pluginInfo ? $this->___callPlugins('getCustomerBeforeAuthUrl', func_get_args(), $pluginInfo) : parent::getCustomerBeforeAuthUrl();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getActionFlagList()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getActionFlagList');
+        return $pluginInfo ? $this->___callPlugins('getActionFlagList', func_get_args(), $pluginInfo) : parent::getActionFlagList();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLoginUrl()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getLoginUrl');
+        return $pluginInfo ? $this->___callPlugins('getLoginUrl', func_get_args(), $pluginInfo) : parent::getLoginUrl();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRedirectActionName()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getRedirectActionName');
+        return $pluginInfo ? $this->___callPlugins('getRedirectActionName', func_get_args(), $pluginInfo) : parent::getRedirectActionName();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function redirectLogin()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'redirectLogin');
+        return $pluginInfo ? $this->___callPlugins('redirectLogin', func_get_args(), $pluginInfo) : parent::redirectLogin();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dispatch(\Magento\Framework\App\RequestInterface $request)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'dispatch');
+        return $pluginInfo ? $this->___callPlugins('dispatch', func_get_args(), $pluginInfo) : parent::dispatch($request);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getActionFlag()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getActionFlag');
+        return $pluginInfo ? $this->___callPlugins('getActionFlag', func_get_args(), $pluginInfo) : parent::getActionFlag();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRequest()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getRequest');
+        return $pluginInfo ? $this->___callPlugins('getRequest', func_get_args(), $pluginInfo) : parent::getRequest();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResponse()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getResponse');
+        return $pluginInfo ? $this->___callPlugins('getResponse', func_get_args(), $pluginInfo) : parent::getResponse();
+    }
+}
